4d0e2a2969c0fcf93b03b52cf7ee8a5fafb52b70
flapp <luisgustavoplathdisaro@gmail.com>
2024-10-10 08:25:09 -0300

    fazendo a page

--- FlapPlathpus/MainPage.xaml.cs
+++ FlapPlathpus/MainPage.xaml.cs
@@ -1,9 +1,42 @@
-﻿namespace FlapPlathpus;
+﻿using Manager;
+
+namespace FlapPlathpus;
 public partial class MainPage : ContentPage
 {
+	const int gravity = 30; // a gravidade que vai ser aplicada no objeto
+
+	const int TimeToFrame = 25; //tempo de espera dos frames ou fps
+
+	bool isDead = false; // Óbvio que é quando o passaro cai pelo cano
+
+//--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	public MainPage()
 	{
 		InitializeComponent();
+		Drawn().RunSynchronously(); 
+		//faz ele rodar de forma sincrona ou junto com a pagina
 	}
+
+    async Task Drawn()
+	{
+		while(!isDead) 
+		// !=não, enquanto não está morto aplica gravidade
+		{
+			IntroGravity();
+		}
+	}
+
+    void IntroGravity()
+	{
+		Imagemperry.TranslationY += gravity; 
+		// translation é a transiçao do eixo Y ou x no caso como aumenta
+		//é oque vai fazer o pasarinho cair 
+		                                       
+	}
+
 }
+
+
+
+
 
--- FlapPlathpus/Manager/Gravity.cs
+++ FlapPlathpus/Manager/Gravity.cs
@@ -2,9 +2,5 @@
 namespace Manager;
 public class Gravity
 {
-    Var A=1;
-    
-     whilhe(A == 1);
-    passarinhoquelevainformacoes.Translation Y(2);
     
 }
