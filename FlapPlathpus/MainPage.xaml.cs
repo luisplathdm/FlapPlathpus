96665db7e6afa9f331f75f75c7766eab1108594f
flap <luisgustavoplathdisaro@gmail.com>
2024-10-11 09:54:54 -0300

    Arrumando tudo oque u perdi ontem

--- FlapPlathpus/MainPage.xaml.cs
+++ FlapPlathpus/MainPage.xaml.cs
@@ -8,9 +8,18 @@ public partial class MainPage : ContentPage
 
 	const int TimeToFrame = 2000; 
 	//tempo de espera dos frames ou fps
-
+    
 	bool isDead = false; 
 	// Óbvio que é quando o passaro cai pelo cano
+
+	double windowHeigth = 0;
+	// è a altura da janela 
+
+	double windowWidth = 0;
+	// è a espessura da janela que vai aparecer
+
+	int Fasty = 30; //60 fps
+	// velocidade de movimento do cano
 
 
 //---------------------------------------------------------------------------------------//
@@ -27,9 +36,15 @@ public partial class MainPage : ContentPage
 		while(!isDead) 
 		// !=não, enquanto não está morto aplica gravidade
 		
-		
 		{
+			ManagerCan();
+			//Colocando que enquanto o passaro não morreu 
+			//aplica a move dos canos tambem
+
 			IntroGravity();
+			//Aqui é a gravidade que a gente atribuiu lá em cima
+			// que vai ser aplicada no passaro
+
 			await Task.Delay(TimeToFrame);
 		    // como esse é o tempo de espra entre os frames 
 		}
@@ -40,9 +55,28 @@ public partial class MainPage : ContentPage
 	async void IntroGravity()
 	{
 		Imgperry.TranslationY += gravity; 
-		// translation é a transiçao do eixo Y ou x no caso como aumenta
-		//é oque vai fazer o pasarinho cair 
-		                                      
+		// translation é a transiçao do eixo Y ou x no caso 
+		//como aumenta é oque vai fazer o pasarinho cair                                     
+	}
+
+//---------------------------------------------------------------------------------------//
+    
+    void ManagerCan()
+	{
+		Imgcanobaixo.TranslationX -= Fasty;
+		Imgcanocima.TranslationX -= Fasty;
+		// aqui a genta está pedindo para que os canos se movam horizontalmente
+		// para a esquerda da janela 
+		
+		if 
+		 (Imgcanobaixo.TranslationX<=-windowWidth)
+		 // aqui é quando a tela acaba entã é redefinido a posiçaõ
+		 // fazendo o cano voltar no ponto X e refazer o processo
+		{
+			Imgcanobaixo.TranslationX = 0;
+			Imgcanocima.TranslationX = 0;
+			// aqui são as imagens com o atributo zerado
+		}
 	}
 
 //---------------------------------------------------------------------------------------//
@@ -53,8 +87,18 @@ public partial class MainPage : ContentPage
 		Drawn();
 		//faz ele rodar de forma sincrona ou junto com a pagina
 	}
-	
-//---------------------------------------------------------------------------------------//
+
+ //---------------------------------------------------------------------------------------//
+    protected override void OnSizeAllocated(double w, double h)
+    {
+        base.OnSizeAllocated(w, h);
+		windowWidth = w;
+		//Espessura da janela
+		windowHeigth = h;
+		// aqui a gente está definindo as janelas para definir o tamanho depois
+    }
+
+    //---------------------------------------------------------------------------------------//
 }
 
 
